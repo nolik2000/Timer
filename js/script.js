c36c641f488fc56800acafd44a2ca63654e74a69
Nastya Kostiv <kostiv08nastya@gmail.com>
2016-10-20 22:42:22 +0300

    Fixed errors and added audio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
+var counter = 0
+
 function start() {
-    function count() {
-        var i = 150;
-        var timer = setInterval(function() {
-            console.log(i);
-            document.getElementById('text').innerHTML = i;
-            if (i == 0){
-              clearInterval(timer);
-            document.getElementById('text').innerHTML = "<div id = 'trailer' class='is_overlay'>  <video autoplay> + <source src = 'vid.mp4' type = 'video/mp4'> + </source> + </video>  </div>" ;
-            }
-            i--;
-        }, 1000);
+    counter += 1
+    if (counter == 1) {
+        function count() {
+            var i = 666;
+            var timer = setInterval(function() {
+                console.log(i);
+                document.getElementById('text').innerHTML = i;
+                document.getElementById('audi').innerHTML = "<audio src='tick.mp3' autoplay loop> </audio>";
+                if (i == 0) {
+                    clearInterval(timer);
+                    document.getElementById('text').innerHTML = "<div id = 'trailer' class='is_overlay'>  <video autoplay> + <source src = 'vid.mp4' type = 'video/mp4'> + </source> + </video>  </div>";
+                    document.getElementById('audi').innerHTML = "<audio src='T_H.mp3' autoplay> </audio>";
+                }
+                i--;
+            }, 1000);
+        }
+        count();
     }
-    count();
 }
